0703363a80362d45edcc9b035e476451b02bc2ff
Lougle <louglescom@gmail.com>
2021-01-26 19:13:51 +0200

    add light and dark theme, finished templated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import recipesData from './data/menu.json';
+import recipesTamplate from './templates/recipes.hbs';
 import './styles.css';
+const markup = recipesTamplate(recipesData);
+document.querySelector(".js-menu").insertAdjacentHTML('beforeend', markup);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const bar = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+bar.addEventListener("change", onLightDarkTheme);
+function onLightDarkTheme() {
+  if (this.checked) {
+      body.setAttribute('class', Theme.DARK);
+  } else {
+      body.setAttribute('class', Theme.LIGHT);
+  }
+};
